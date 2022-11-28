a5f6b929219c8bb1799fe43d67f5781fb9bd9c99
John Luo <jtluo@andrew.cmu.edu>
2022-11-28 18:28:19 -0500

    Baseline concurrent queue implementation and benchmarks (#1)
    * Reference algorithms for CPQ
    
    * Reduce size of init files and record runtimes for reference
    
    * Add checker code for correctness and speedup
    
    * Initial parDijk implementation
    
    Functionality not yet complete
    
    * Fix concensus bug

---
+++ checker.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import re
+
+script_dir = os.path.dirname(__file__)
+graph_dir = os.path.join(script_dir, "graphs")
+runtime_ref_path = os.path.join(graph_dir, "runtime-ref.txt")
+
+# Usage: ./checker.py
+prog = 'pardijk-release'
+if os.environ["HOME"].startswith("/afs/andrew.cmu.edu"):
+    workers = [4, 8]
+else:
+    workers = [16, 128]
+
+ref_perfs = {}
+
+with open(runtime_ref_path, "r") as f:
+    lines = f.readlines()
+
+for line in lines:
+    scene, runtime = line.split(" ")
+    ref_perfs[scene] = float(runtime)
+
+perfs = [[None] * len(workers) for _ in range(len(ref_perfs))]
+
+def compare(actual, ref):
+    threshold = 1.0 if 'repeat' in actual else 0.1
+    actual = open(actual).readlines()
+    ref = open(ref).readlines()
+    assert len(actual) == len(ref), \
+        f'ERROR -- number of verticies is {len(actual)}, should be {len(ref)}'
+    for i, (l1, l2) in enumerate(zip(actual, ref)):
+        l1 = [int(x) for x in l1.split()]
+        l2 = [int(x) for x in l2.split()]
+        assert len(l1) == len(l2),\
+            f'ERROR -- invalid format at line {i}, should contain {len(l2)} ints'
+
+        for j, (x, y) in enumerate(zip(l1, l2)):
+            assert abs(x - y) < threshold, f'ERROR -- incorrect result at line {i} value {j}, expected value {y} actual value {x}'
+
+def compute_score(actual, ref):
+    return ref/actual
+
+os.system('mkdir -p logs')
+os.system('rm -rf logs/*')
+for i, scene_name in enumerate(ref_perfs.keys()):
+    for j, worker in enumerate(workers):
+        print(f'--- running {scene_name} on {worker} workers ---')
+        init_file = f'graphs/{scene_name}-init.txt'
+        output_file = f'logs/{scene_name}-{worker}.txt'
+        log_file = f'logs/{scene_name}-{worker}.log'
+        cmd = f'OMP_NUM_THREADS={worker} ./{prog} -in {init_file} -o {output_file} > {log_file}'
+        print(f'Command: {cmd}')
+        ret = os.system(cmd)
+        assert ret == 0, 'ERROR -- nbody exited with errors'
+        compare(output_file, f'graphs/{scene_name}-ref.txt')
+        t = float(re.findall(
+            r'total time: (.*?)s', open(log_file).read())[0])
+        print(f'total time: {t:.6f}s')
+        perfs[i][j] = t
+
+print('\n-- Performance Table ---')
+header = '|'.join(f' {x:<15} ' for x in ['Scene Name'] + workers)
+print(header)
+print('-' * len(header))
+for scene, perf in zip(ref_perfs.keys(), perfs):
+    print('|'.join(f' {x:<15} ' for x in [scene] + perf))
+
+score = 0.0
+print('\n-- Speedup Table ---')
+print(header)
+print('-' * len(header))
+for i, (scene, perf) in enumerate(zip(ref_perfs.items(), perfs)):
+    scores = [compute_score(p, scene[1])
+              for p in perf]
+    scores_str = [f"{s:.6f}" for s in scores]
+    score += sum(scores)
+    print('|'.join(f' {x:<15} ' for x in [scene[0]] + scores_str))
+print(f'average speedup: {score/(len(workers) * len(ref_perfs))}')
---
+++ generate_benchmarks.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import os
+import random
+import re
+
+connectivity = 0.05
+
+script_dir = os.path.dirname(__file__)
+graph_dir = os.path.join(script_dir, "graphs")
+ref_executable = os.path.join(script_dir, "dijk-release")
+runtime_ref_path = os.path.join(graph_dir, "runtime-ref.txt")
+
+graph_sizes = [64, 256, 1024, 4096, 8192]
+runtime_ref = []
+
+for graph_size in graph_sizes:
+    possible_edges = (graph_size*(graph_size-1)//2)
+    num_edges = (int)(possible_edges * connectivity)
+    edges = [0] * possible_edges
+    for i in range(num_edges):
+        edges[i] = 1
+    random.shuffle(edges)
+    edge_index = 0
+
+    graph_path = os.path.join(graph_dir, f"bench-{graph_size}-init.txt")
+    print(f"Generating graph of size {graph_size} at {graph_path}")
+    with open(graph_path, "w") as f:
+        f.write(f"{graph_size}\n")
+        for i in range(graph_size-1):
+            for j in range(i+1, graph_size):
+                if (edges[edge_index] == 1):
+                    f.write(f"{i} {j} {random.randint(1, 99)}\n")
+                edge_index += 1
+
+    ref_path = os.path.join(graph_dir, f"bench-{graph_size}-ref.txt")
+    log_path = f'logs/bench-{graph_size}.log'
+    print(f"Generating reference at {ref_path}")
+    cmd = f'{ref_executable} -in {graph_path} -o {ref_path} > {log_path}'
+    print(f'Command: {cmd}')
+    ret = os.system(cmd)
+    t = float(re.findall(
+        r'total time: (.*?)s', open(log_path).read())[0])
+    print(f'total time: {t:.6f}s')
+    runtime_ref.append(t)
+
+with open(runtime_ref_path, "w") as f:
+    for i in range(len(graph_sizes)):
+        f.write(f"bench-{graph_sizes[i]} {runtime_ref[i]:.6f}\n")
