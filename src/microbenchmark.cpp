c2c84b051cd940870ae419ff8aaa0bd88f7d34b1
yuminc1234 <yuminc@ghc35.ghc.andrew.cmu.edu>
2022-12-15 21:24:52 -0500

    Remove dead code

--- src/microbenchmark.cpp
+++ src/microbenchmark.cpp
@@ -8,6 +8,7 @@
 #include "timing.h"
 #include <queue>
 #include "pardijk.cpp"
+#include <omp.h>
 using namespace std;
 
 // reference: https://stackoverflow.com/questions/36922371/generate-different-random-numbers
@@ -17,10 +18,10 @@ vector<int> generateRandNum(int size) {
         numbers.push_back(i + 1);
     }
     ofstream randomFile("../microbenchmark/random-ref.txt");
-    for (int i = 0; i < size; i++) {
-        randomFile << numbers[i] << endl;
-    }
-    randomFile.close();
+    // for (int i = 0; i < size; i++) {
+    //     randomFile << numbers[i] << endl;
+    // }
+    // randomFile.close();
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     shuffle(numbers.begin(), numbers.end(), default_random_engine(seed));
     return numbers;
@@ -28,14 +29,14 @@ vector<int> generateRandNum(int size) {
 
 void insertDelete(vector<int>& randNums, PriorityQueue<8, 1000001, 100, 0, int, int>* pq) {
     Timer t;
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(static, 1)
     for (int i = 0; i < randNums.size(); i++) {
         pq->insert(randNums[i], 0);
     }
     cout << "insert time is: " << t.elapsed() << endl;
     t.reset();
 
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(static, 1)
     for (int i = 0; i < randNums.size(); i++) {
         auto currMin = pq->deleteMin();
         // cout << omp_get_thread_num() << " " << get<0>(currMin) << " " << get<1>(currMin) << " " << get<2>(currMin) << endl;
@@ -61,13 +62,13 @@ void insertDeleteSeq(vector<int>& randNums, priority_queue<int, vector<int>, gre
 
 void insertDeleteCoarse(vector<int>& randNums, GlobalLockPQ* pq) {
     Timer t;
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(static, 1)
     for (int i = 0; i < randNums.size(); i++) {
         pq->insert(Offer(i, randNums[i]));
     }
     cout << "globalLock insert time is: " << t.elapsed() << endl;
     t.reset();
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(static, 1)
     for (int i = 0; i < randNums.size(); i++) {
         pq->deleteMin();
     }
